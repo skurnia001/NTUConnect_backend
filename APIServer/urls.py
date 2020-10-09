6fb013ccf2db6969315b168a8dc80d3f92464c21
kevintarjono <kevin.tarjono@gmail.com>
2020-10-09 23:21:27 +0800

    See specific threads and See threads in specific course API

--- APIServer/urls.py
+++ APIServer/urls.py
@@ -1,15 +1,26 @@
 # users/urls.py
 from django.urls import path
 
-from .views import UserList, UserDetail, ForumCreation, ThreadCreation, ThreadList, ForumList, MessageCreation, ThreadSpecific
+from .views import (
+    UserList,
+    UserDetail,
+    ForumCreation,
+    ForumSpecific,
+    ForumList,
+    ThreadCreation,
+    ThreadList,
+    ThreadSpecific,
+    MessageCreation
+)
 
 urlpatterns = [
     path('users/', UserList.as_view()),
     path('users/<int:pk>/', UserDetail.as_view()),
     path('createforums/', ForumCreation.as_view()),
     path('forumlist/', ForumList.as_view()),
+    path('forumspec/<int:pk>', ForumSpecific.as_view()),
     path('createthreads/', ThreadCreation.as_view()),
-    path('threadlist/<int:pk>/', ThreadList.as_view()),
+    path('threadlist/', ThreadList.as_view()),
     path('threadspec/<int:pk>/', ThreadSpecific.as_view()),
     path('createmessages/', MessageCreation.as_view()),
 ]
